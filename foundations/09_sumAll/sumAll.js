e1158abae39c0d3ae6de50dc2108a56aad3e7d0e
jackdev-99 <jacklinmalek@gmail.com>
2026-04-15 16:23:26 +0200

    Complete using for loop with math method

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,10 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    for(i=Math.min(num1,num2); i<= Math.max(num1,num2); i++ ) {
+        sum += i;
+    }
+    return sum;
 };
-
+console.log(sumAll(1,4)); // returns the sum of 1 + 2 + 3 + 4 which is 10
 // Do not edit below this line
 module.exports = sumAll;
